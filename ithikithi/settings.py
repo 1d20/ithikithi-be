e9cf85fa1e876eeef290483e3ac8c210a8faf3c6
Andriy Letsyk <andriyletsyk@Andriys-MacBook-Pro.local>
2017-12-03 03:13:14 +0200

    Add ticket model

--- ithikithi/settings.py
+++ ithikithi/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'person.apps.PersonConfig',
     'booking.apps.BookingConfig',
+    'ticket.apps.TicketConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
