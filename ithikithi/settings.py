9bfd2dce6c96eb186116dc18c315588af8d2bd4d
Andriy Letsyk <andriyletsyk@Andriys-MacBook-Pro.local>
2017-12-03 02:57:40 +0200

    Add two fields to person model and create new booking app with models

--- ithikithi/settings.py
+++ ithikithi/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'person.apps.PersonConfig',
+    'booking.apps.BookingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
