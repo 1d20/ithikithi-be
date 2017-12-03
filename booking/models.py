259ba8691f43bfee5097bdd673162ea6813ebeb7
Andriy Letsyk <andriyletsyk@Andriys-MacBook-Pro.local>
2017-12-03 02:58:20 +0200

    Add losted files

---
+++ booking/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Booking(models.Model):
+    user_id = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.email}'
+
+
+class BookingPerson(models.Model):
+    booking_id = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    from_location = models.CharField(max_length=1000) # TODO change to JsonField for PostgreSQL
+    to_location = models.CharField(max_length=1000) # TODO change to JsonField for PostgreSQL
+    date = models.DateTimeField(default=timezone.now())
+    train_number = models.CharField(max_length=8)
+    wagon = models.CharField(max_length=5)
+    place = models.CharField(max_length=5)
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    email = models.EmailField()
+    student_card_number = models.CharField(max_length=10)
+    created_on = models.DateTimeField(default=timezone.now())
+    need_to_change_ticket = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.email} {self.train_number} {self.wagon} {self.place}'
