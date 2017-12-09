14bf19d6fe29fe1dfb56cc96799c1022f0f7b8ad
Andriy Letsyk <andriyletsyk@Andriys-MacBook-Pro.local>
2017-12-09 21:32:47 +0200

    Add get list of trains

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions as django_permissions
+from rest_framework import viewsets, status, permissions as django_permissions
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 from uz_api import ClientInteface, Serializer
@@ -30,4 +30,28 @@ class UZViewSet(viewsets.ViewSet):
     @list_route(methods=['get'])
     def stations(self, request):
         stations = client.stations(name=request.query_params.get('name', ''))
-        return Response(data=Serializer.serialize(stations))
+        return Response(
+            data=Serializer.serialize(stations),
+            status=status.HTTP_200_OK,
+        )
+
+    @list_route(methods=['get'])
+    def trains(self, request):
+        request_params = {}
+        get_data = request.query_params
+        required_fields = ['station_from_id', 'station_to_id', 'date_dep']
+        if all([attr in get_data for attr in required_fields]):
+            enable_params = required_fields + ['time_dep', 'time_dep_till', 'another_ec', 'search']
+            for attr, val in get_data.items():
+                if attr in enable_params:
+                    request_params[attr] = val
+            result = client.trains(**request_params)
+            return Response(
+                data=Serializer.serialize(result),
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                data='bad_request',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
