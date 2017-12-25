32e6d840ee1a503e17526d57f7e8e43d23c0d8f2
Andriy Letsyk <andriyletsyk@Andriys-MacBook-Pro.local>
2017-12-25 10:21:41 +0200

    Refactor code

--- person/views.py
+++ person/views.py
@@ -22,26 +22,31 @@ class PersonViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         data = request.data
         serializer = self.serializer_class(data=data)
-        if serializer.is_valid():
-            email_exist = models.Person.objects.filter(user_id=request.user, email=data.get('email')).exists()
-            if not email_exist:
-                person = models.Person()
-                for attr in ['first_name', 'last_name', 'email', 'student_card_number']:
-                    setattr(person, attr, data.get(attr))
-                person.user_id = models.CustomUser.objects.get(user_ptr_id=request.user.id)
-                person.save()
-                return Response(
-                    data=serializers.PersonSerializer(person).data,
-                    status=status.HTTP_201_CREATED,
-                )
+        if not serializer.is_valid():
+            return Response(
+                data='invalid_data',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        email_exist = models.Person.objects.filter(user_id=request.user, email=data.get('email')).exists()
+        if email_exist:
             return Response(
                 data='already_added',
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+        person = models.Person()
+        for attr in ['first_name', 'last_name', 'email', 'student_card_number']:
+            setattr(person, attr, data.get(attr))
+
+        person.user_id = models.CustomUser.objects.get(user_ptr_id=request.user.id)
+        person.save()
         return Response(
-            data='invalid_data',
-            status=status.HTTP_400_BAD_REQUEST,
+            data=serializers.PersonSerializer(person).data,
+            status=status.HTTP_201_CREATED,
         )
+
+
 
     @list_route(methods=['get'])
     def get_queryset(self):
@@ -74,53 +79,54 @@ class AuthenticationViewSet(viewsets.ViewSet):
     def signup(self, request):
         data = request.data
         serializer = self.serializer_class(data=data)
-        if serializer.is_valid():
-            user = models.CustomUser.objects.filter(email=data['email'])
-            if not user:
-                user = models.CustomUser.objects.create_user(
-                    username=data['email'],
-                    email=data['email'],
-                    password=data['password'],
-                )
-                token = secrets.token_urlsafe(20)
-                send_mail(
-                    subject='Test',
-                    message='''Test %s ''' % (request.build_absolute_uri('/') + 'api/person/authenticate/confirm/' + token),
-                    from_email=EMAIL_HOST_USER,
-                    recipient_list=[data['email']],
-                    fail_silently=False
-                )
-                user.token = token
-                user.confirm_time = timezone.now()
-                user.save()
-                return Response(
-                    data=serializers.CustomUserSerializer(user).data,
-                    status=status.HTTP_201_CREATED,
-                )
-            else:
-                return Response(
-                    data='user_already_exists',
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-        else:
+        if not serializer.is_valid():
             return Response(
                 data='validation_error',
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+        user = models.CustomUser.objects.filter(email=data['email'])
+        if user:
+            return Response(
+                data='user_already_exists',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user = models.CustomUser.objects.create_user(
+            username=data['email'],
+            email=data['email'],
+            password=data['password'],
+        )
+        token = secrets.token_urlsafe(20)
+        send_mail(
+            subject='Test',
+            message='''Test %s ''' % (request.build_absolute_uri('/') + 'api/person/authenticate/confirm/' + token),
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[data['email']],
+            fail_silently=False
+        )
+        user.token = token
+        user.confirm_time = timezone.now()
+        user.save()
+        return Response(
+            data=serializers.CustomUserSerializer(user).data,
+            status=status.HTTP_201_CREATED,
+        )
 
     @list_route(methods=['get'])
     @permission_classes(django_permissions.IsAuthenticated)
     def logout(self, request):
         user = request.user
         if user.is_authenticated:
-            logout(request)
             return Response(
-                data='OK',
-                status=status.HTTP_200_OK,
+                data='guest user',
+                status=status.HTTP_400_BAD_REQUEST,
             )
+
+        logout(request)
         return Response(
-            data='guest user',
-            status=status.HTTP_400_BAD_REQUEST,
+            data='OK',
+            status=status.HTTP_200_OK,
         )
 
     @list_route(methods=['get'], url_path='confirm/(?P<token>[a-zA-Z0-9_-]+)')
@@ -133,14 +139,15 @@ class AuthenticationViewSet(viewsets.ViewSet):
             is_confirmed=False,
             confirm_time__range=((current_datetime - timedelta(days=days_link_enable)), current_datetime)
         )
-        if user and len(user) == 1:
-            user[0].is_active = True
-            user[0].save()
+        if not (user and len(user) == 1):
             return Response(
-                data='OK',
-                status=status.HTTP_200_OK,
+                data='FAIL',
+                status=status.HTTP_400_BAD_REQUEST,
             )
+
+        user[0].is_active = True
+        user[0].save()
         return Response(
-            data='FAIL',
-            status=status.HTTP_400_BAD_REQUEST,
+            data='OK',
+            status=status.HTTP_200_OK,
         )
